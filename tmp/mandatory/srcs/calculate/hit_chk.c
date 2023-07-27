3c4f90bbf473ea0f4c86832df690912b8e37c081
cryscham123 <cryscham123@cryscham123>
2023-07-28 07:43:02 +0900

    tmp

--- tmp/mandatory/srcs/calculate/hit_chk.c
+++ tmp/mandatory/srcs/calculate/hit_chk.c
@@ -59,48 +59,6 @@ static int	hit_chk_pl(t_ray *ray, t_plane *pl, t_rec *rec)
 
 static int	hit_chk_cy(t_ray *ray, t_cylinder *cy, t_rec *rec)
 {
-    double  a;
-		double  half_b;
-    double  c;
-    t_vec3  u;
-    t_vec3  o;
-    t_vec3  delta_P;
-    double r;
-
-    double discriminant; // 판별식
-    double sqrtd;
-    double root;
-    double hit_height;
-    
-    cy = cy_obj->element;
-    u = ray->dir;
-    o = cy->dir;
-    r = cy->diameter / 2;
-    delta_P = vminus(ray->origin, cy->center);
-    a = vlength2(vcross(u, o));
-    half_b = vdot(vcross(u, o), vcross(delta_P, o));
-    c = vlength2(vcross(delta_P, o)) - pow(r, 2);
-    discriminant = half_b * half_b - a * c;
-    if (discriminant < 0) 
-        return (FALSE);
-    // 이 시점에서 판별식이 참이 나왔기에 근이 존재한다고 판단한다.
-    sqrtd = sqrt(discriminant); 
-    root = (-half_b - sqrtd) / a;  // 근의 공식 해, 작은 근부터 고려.
-    if (root < rec->tmin || rec->tmax < root)
-    {
-    root = (-half_b + sqrtd) / a; 
-        if (root < rec->tmin || rec->tmax < root)
-        return (FALSE);
-    }
-    if (!(hit_height = cy_boundary(cy, ray_at(ray, root))))
-        return (FALSE);
-
-    rec->t = root; // 광선의 원점과 교점까지의 거리를 rec에 저장한다.
-    rec->p = ray_at(ray, root); // 교점의 좌표를 rec에 저장한다.
-    rec->normal = get_cylinder_normal(cy, rec->p, hit_height); // vmult(ray->dir, root)하면 안돼!!!
-    set_face_normal(ray, rec);
-    rec->albedo = cy_obj->albedo;
-    return (TRUE);
 }
 
 int	is_hit(t_ray *ray, t_lst *objs, t_rec *rec)
